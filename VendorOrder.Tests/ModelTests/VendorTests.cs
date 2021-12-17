b8cba88dc00dbf351d238863dc819a7353fa40cc
Albert Lee <leesga8@gmail.com>
2021-12-17 14:54:58 -0800

    pass test for property retrieval and Id

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,47 @@ using System;
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("test", "Vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    }
+    }  [TestMethod]
+  public void GetName_ReturnsName_String()
+  {
+    //Arrange
+    string name = "Test";
+    string description = "Vendor";
+    Vendor newVendor = new Vendor(name, description);
+
+    //Act
+    string result = newVendor.Name;
+
+    //Assert
+    Assert.AreEqual(name, result);
+  }
+
+  [TestMethod]
+  public void GetId_ReturnsVendorId_Int()
+  {
+    //Arrange
+    string name = "Test";
+    string description = "Vendor";
+    Vendor newVendor = new Vendor(name, description);
+
+    //Act
+    int result = newVendor.Id;
+
+    //Assert
+    Assert.AreEqual(1, result);
+  }
+
   }
 }
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -4,7 +4,7 @@ namespace VendorOrder.Models
 {
   public class Vendor
   {
-    private static List<Vendor> _instances = new List<Vendor> {};
+    private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
@@ -15,7 +15,12 @@ namespace VendorOrder.Models
       Description = description;
       _instances.Add(this);
       Id = _instances.Count;
-      Orders = new List<Order>{};
+      Orders = new List<Order> { };
     }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
   }
 }

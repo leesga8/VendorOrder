e5736a658e49da8015c14b578834693ed8ba8df0
Albert Lee <leesga8@gmail.com>
2021-12-17 13:47:42 -0800

    add OrdersController, form, displays list

--- VendorOrder/Controllers/OrdersController.cs
+++ VendorOrder/Controllers/OrdersController.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorOrder.Models;
+using System.Collections.Generic;
 
 namespace VendorOrder.Controllers
 {
-  public class HomeController : Controller
+  public class OrdersController : Controller
   {
 
-    [Route("/")]
+    [HttpGet("/orders")]
     public ActionResult Index()
     {
-      Order starterOrder = new Order("string","string", 0, "string");
-      return View(starterOrder);
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(string title, string description, int price, string date)
+    {
+      Order myOrder = new Order(title, description, price, date);
+      return RedirectToAction("Index");
     }
 
   }

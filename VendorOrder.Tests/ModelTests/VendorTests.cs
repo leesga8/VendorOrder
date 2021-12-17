0f1460b15900b874eaf799b48683b2da58b31483
Albert Lee <leesga8@gmail.com>
2021-12-17 14:48:23 -0800

    pass test, VendorConstructor instantiation

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test", "Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ VendorOrder/Models/Vendor.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace VendorOrder.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+  }
+}

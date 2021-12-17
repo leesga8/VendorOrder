e4cd6d35802b2f3f66883d88b4a58c48af64a939
Albert Lee <leesga8@gmail.com>
2021-12-17 12:33:20 -0800

    create constructor. 2nd test Good fail RED

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -9,8 +9,23 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceofOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test","test1",1,"test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    } 
+    
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      //Arrange
+      string title = "bread";
+      string description = "sourdough loaf";
+      int price = 5;
+      string date = "1/1/2001";
+      Order newOrder = new Order(title, description, price, date);
+      //Act
+      string result = newOrder.Title;
+      //Assert
+      Assert.AreEqual("test", result);
     }
   }
 }
